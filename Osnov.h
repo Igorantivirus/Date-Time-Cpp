53ef8f09689ed6f6f4e4bf7cfdab28ba3cc314ef
Igorantivirus <125749625+Igorantivirus@users.noreply.github.com>
2023-02-26 19:10:51 +0700

    Улучшения и добавления

--- Osnov.h
+++ Osnov.h
@@ -3,6 +3,56 @@
 #include<string>
 using namespace std;
 namespace kak_u_Igorya {
+	template<typename R>
+	inline void revers(R& a, R& b) { auto pr = a; a = b; b = pr; }
+	//vvvvDopolneniya
+	bool prov(string ch) {
+		for (int i = 0; i < ch.size(); i++) { if (!((i >= 48 && i <= 57) || (i == 46) || (i == 45))) { return 0; } }
+		return 1;
+	}
+	int poisk(int* mas, int l, int r) {// 1 3 2 7 9 8 6 5 // 0 8
+		int x = mas[r];
+		int less = l;
+		for (int i = l; i < r; ++i) {
+			if (mas[i] <= x) {
+				revers(mas[i], mas[less]);
+				++less;
+			}
+		}
+		revers(mas[less], mas[r]);
+		return less;
+	}
+	void bis_sor(int* mas, int l, int r) {
+		if (l < r) {
+			int q = poisk(mas, l, r);
+			bis_sor(mas, l, q - 1);
+			bis_sor(mas, q + 1, r);
+		}
+	}
+	void sli_sor(int* values, int* buffer, int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sli_sor(values, buffer, l, m);
+			sli_sor(values, buffer, m + 1, r);
+			int k = l;
+			for (int i = l, j = m + 1; i <= m || j <= r; ) {
+				if (j > r || (i <= m && values[i] < values[j])) {
+					buffer[k] = values[i];
+					++i;
+				}
+				else {
+					buffer[k] = values[j];
+					++j;
+				}
+				++k;
+			}
+			for (int i = l; i <= r; ++i) {
+				values[i] = buffer[i];
+			}
+		}
+	}
+	///\/\/\/\/\Dopolneniya
+	//input
 	string input(string txt = "") {//Обычная функция инпут, как у питона!
 		SetConsoleCP(1251);
 		SetConsoleOutputCP(1251);
@@ -43,7 +93,7 @@ namespace kak_u_Igorya {
 		cin >> ch;
 		return ch;
 	}
-
+	//okrugleniye
 	float round(float ch, int num = 0) {//Эта и последующие - округление как в питоне, указываем первый эл-т - число для округления, вторую - сколько знаков после запятой
 		ch = ((int(ch * pow(10, num + 1)) % 10) >= 5) ? ((int(ch * pow(10, num)) + 1) / (pow(10, num))) : ((int(ch * pow(10, num))) / (pow(10, num)));
 		return ch;
@@ -52,7 +102,7 @@ namespace kak_u_Igorya {
 		ch = ((int(ch * pow(10, num + 1)) % 10) >= 5) ? ((int(ch * pow(10, num)) + 1) / (pow(10, num))) : ((int(ch * pow(10, num))) / (pow(10, num)));
 		return ch;
 	}
-
+	//preobrazovanie
 	int toint(string ch, int els = 0) {//преобразует строку в инт
 		int otv = 0, zn = 1, in = 0;
 		if (ch[0] == '-') { in = 1; zn = -1; }
@@ -61,41 +111,26 @@ namespace kak_u_Igorya {
 		}
 		return ((to_string(otv * zn) == ch) ? (otv * zn) : (els));
 	}
-	float tofloat(string ch, float els = 0) {//преобразует строку в флот
-		int len = ch.size(), in = len, zn = 1, nach = 0;
-		if (ch[0] == '-') { zn = -1, nach = 1; }
-		for (int i = nach; i < len; i++) {
-			if (ch[i] == '.' || ch[i] == ',') { in = i; break; }
+	float tofloat(string ch, float els = 0) {
+		if (!prov(ch)) { return els; }
+		int ot = 0, zn = 1, len = ch.size(), zap = len; float otv = 0; bool da = 0;
+		if (ch[0] == '-') { zn = -1; ot = 1; }
+		for (int i = ot; i < len; i++) {
+			if (da) { otv += (int(ch[i]) - 48) / pow(10, i - zap); }
+			if (ch[i] == '.' || ch[i] == ',') { zap = i; da = 1; }
 		}
-		float otv = 0;
-		for (int i = nach; i < in; i++) {
-			otv += (int(ch[i]) - 48) * pow(10, in - i - 1);
-		}
-		for (int i = in + 1; i < len; i++) {
-			otv += float(int(ch[i] - 48) / pow(10, i - in));
-		}
-		int sum = 0;
-		for (int i = in + 1; i < len; i++) { sum++; }
-		while (sum < 6) {
-			ch += "0";
-			sum++;
-		}
-		//return ((to_string(otv * zn) == ch) ? (otv * zn) : (els));
+		for (int i = ot; i < zap; i++) { otv += (int(ch[i]) - 48) * pow(10, zap - i - 1); }
 		return otv * zn;
 	}
-	double todouble(string ch) {//преобразует строку в дабл
-		int len = ch.size(), in = len, zn = 1, nach = 0;
-		if (ch[0] == '-') { zn = -1, nach = 1; }
-		for (int i = nach; i < len; i++) {
-			if (ch[i] == '.') { in = i; break; }
+	double todouble(string ch, double els = 0) {
+		if (!prov(ch)) { return els; }
+		int ot = 0, zn = 1, len = ch.size(), zap = len; double otv = 0; bool da = 0;
+		if (ch[0] == '-') { zn = -1; ot = 1; }
+		for (int i = ot; i < len; i++) {
+			if (da) { otv += (int(ch[i]) - 48) / pow(10, i - zap); }
+			if (ch[i] == '.' || ch[i] == ',') { zap = i; da = 1; }
 		}
-		double otv = 0;
-		for (int i = nach; i < in; i++) {
-			otv += (int(ch[i]) - 48) * pow(10, in - i - 1);
-		}
-		for (int i = in + 1; i < len; i++) {
-			otv += double(int(ch[i] - 48) / pow(10, i - in));
-		}
+		for (int i = ot; i < zap; i++) { otv += (int(ch[i]) - 48) * pow(10, zap - i - 1); }
 		return otv * zn;
 	}
 	bool tobool(string ch, bool els = 1) {//преобразует строку в бул
@@ -105,74 +140,81 @@ namespace kak_u_Igorya {
 			return els;
 		}
 	}
-
-	int maxof(int* mas, int len) {//находит максимальный эл-т массива из интов
+	//bolshe minshe
+	int maxof(int* mas, int len, int ot = 0) {//находит максимальный эл-т массива из интов
 		int ma = mas[0];
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (mas[i] > ma) ma = mas[i];
 		}
 		return ma;
 	}
-	float maxof(float* mas, int len) {//находит максимальный эл-т массива из флот
+	float maxof(float* mas, int len, int ot = 0) {//находит максимальный эл-т массива из флот
 		float ma = mas[0];
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (mas[i] > ma) ma = mas[i];
 		}
 		return ma;
 	}
-	double maxof(double* mas, int len) {//находит максимальный эл-т массива из дабл
+	double maxof(double* mas, int len, int ot = 0) {//находит максимальный эл-т массива из дабл
 		double ma = mas[0];
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (mas[i] > ma) ma = mas[i];
 		}
 		return ma;
 	}
-	string maxof(string* mas, int len) {//находит максимальный эл-т массива из стринг
+	string maxof(string* mas, int len, int ot = 0) {//находит максимальный эл-т массива из стринг
 		int ma = mas[0].size();
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (ma < mas[0].size()) {
 				ma = mas[0].size();
 			}
 		}
 		return mas[ma];
 	}
-
-	int minof(int* mas, int len) {//эта и последующая  - то же самое но для инт
+	int minof(int* mas, int len, int ot = 0) {//эта и последующая  - то же самое но для инт
 		int ma = mas[0];
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (mas[i] < ma) ma = mas[i];
 		}
 		return ma;
 	}
-	float minof(float* mas, int len) {
+	float minof(float* mas, int len, int ot = 0) {
 		float ma = mas[0];
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (mas[i] < ma) ma = mas[i];
 		}
 		return ma;
 	}
-	double minof(double* mas, int len) {
+	double minof(double* mas, int len, int ot = 0) {
 		double ma = mas[0];
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (mas[i] < ma) ma = mas[i];
 		}
 		return ma;
 	}
-	string minof(string* mas, int len) {
+	string minof(string* mas, int len, int ot = 0) {
 		int ma = mas[0].size();
-		for (int i = 0; i < len; i++) {
+		for (int i = ot; i < len; i++) {
 			if (ma > mas[0].size()) {
 				ma = mas[0].size();
 			}
 		}
 		return mas[ma];
 	}
-
+	//rabota s massivami
 	template<typename T>
-	void print_arr(T* mas, int len, string pr = " ") {//Выводит масив
+	void print_arr(T* mas, int len, string pr = ", ", string end = "\n") {//Выводит масив
 		cout << mas[0];
 		for (int i = 1; i < len; i++) {
 			cout << pr << mas[i];
+		}
+		cout << end;
+	}
+	void rand_arr(char* mas, int len, int ot = 0, int doo = 100) {//Заполняет интовский массив, принимая значения от какого и до какого, дальнейшие заполняют  массивы других переменных
+		srand(time(NULL));
+		doo = doo - ot;
+		for (int i = 0; i < len; i++) {
+			mas[i] = char(rand() % doo + ot);
 		}
 	}
 	void rand_arr(int* mas, int len, int ot = 0, int doo = 100) {//Заполняет интовский массив, принимая значения от какого и до какого, дальнейшие заполняют  массивы других переменных
@@ -197,5 +239,190 @@ namespace kak_u_Igorya {
 			v = rand() % (int)pow(10, col);
 			mas[i] = ot + (v / pow(10, col)) * (doo - ot);
 		}
+	}
+	template<typename S>
+	int set(S* mas, int len, int els = 0) {
+		S* newmas = new S[len];
+		int in = 0;
+		bool da = 1;
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < in; j++) {
+				if (mas[i] == newmas[j]) {
+					da = 0;
+				}
+			}
+			if (da) {
+				newmas[in] = mas[i];
+				in++;
+			}
+			da = 1;
+		}
+		for (int i = 0; i < len; i++) {
+			if (i < in) {
+				mas[i] = newmas[i];
+			}
+			else {
+				mas[i] = els;
+			}
+		}
+		delete[] newmas;
+		return in;
+	}
+	template<typename B>
+	void revers_arr(B* mas, int len) {
+		int len2 = ((len % 2 == 0) ? (len / 2) : ((len / 2) + 1));
+		for (int i = 0; i < len2; i++) {
+			revers(mas[i], mas[len - i - 1]);
+		}
+	}
+	template<typename S>
+	S sum(S* mas, int len = 0) {
+		auto s = mas[0];
+		for (int i = 1; i < len; i++) {
+			s += mas[i];
+		}
+		return s;
+	}
+	template<typename U>
+	U umn(U* mas, int len = 0) {
+		auto s = mas[0];
+		for (int i = 1; i < len; i++) {
+			s *= mas[i];
+		}
+		return s;
+	}
+	template<typename S>
+	S sr_arif(S* mas, int len = 0) {
+		auto s = mas[0];
+		for (int i = 1; i < len; i++) {
+			s += mas[i];
+		}
+		return s/len;
+	}
+	template<typename S>
+	int count_arr(S* mas, int len, S ind) {
+		int count = 0;
+		for (int i = 0; i < len; i++) {
+			if (mas[i] == ind) { count++; }
+		}
+		return count;
+	}
+	template<typename T>
+	int index_arr(T* mas, int len, T ch, int ot = 0) {
+		for (int i = ot; i < len; i++) {
+			if (mas[i] == ch) return i;
+		}
+	}
+	//sortirovka massovov
+	template<typename S>
+	void sort_puz(S* mas, int len) {//sortirovka puzirkom
+		int count = 0;
+		for (int i = 0; i + 1 < len; ++i) {
+			for (int j = 0; j + 1 < len - i; ++j) {
+				if (mas[j + 1] < mas[j]) {
+					revers(mas[j], mas[j + 1]);
+					count++;
+				}
+			}
+			if (count == 0) { break; }
+			count = 0;
+		}
+	}
+	template<typename S>
+	void sort_she(S* mas, int len) {//sheikernaya sortirivka
+		if (len == 0) {
+			return;
+		}
+		int left = 0;
+		int right = len - 1;
+		while (left <= right) {
+			for (int i = right; i > left; --i) {
+				if (mas[i - 1] > mas[i]) {
+					revers(mas[i - 1], mas[i]);
+				}
+			}
+			++left;
+			for (int i = left; i < right; ++i) {
+				if (mas[i] > mas[i + 1]) {
+					revers(mas[i], mas[i + 1]);
+				}
+			}
+			--right;
+		}
+	}
+	template<typename S>
+	void sort_ras(S* mas, int len) {//sjrtirovka rashoskoi
+		const double factor = 1.247;
+		double step = len - 1;
+		while (step >= 1) {
+			for (int i = 0; i + step < len; ++i) {
+				if (mas[i] > mas[i + int(step)]) {
+					revers(mas[i], mas[i + int(step)]);
+				}
+			}
+			step /= factor;
+		}
+		for (int i = 0; i + 1 < len; ++i) {
+			for (int j = 0; j + 1 < len - i; ++j) {
+				if (mas[j + 1] < mas[j]) {
+					revers(mas[j], mas[j + 1]);
+				}
+			}
+		}
+	}
+	template<typename S>
+	void sort_vst(S* mas, int len) {//sortirovka vstavkami
+		int x = 0, j = 0;
+		for (int i = 1; i < len; ++i) {
+			x = mas[i];
+			j = i;
+			while (j > 0 && mas[j - 1] > x) {
+				mas[j] = mas[j - 1];
+				--j;
+			}
+			mas[j] = x;
+		}
+	}
+	template<typename S>
+	void sort_vib(S* mas, int len) {//viborochnaya sortirovka
+		auto mini = mas;
+		for (int i = 0; i < len; i++) {
+			for (int j = i; j < len; j++) {
+				if (*(mas + j) < *mini) { mini = (mas + j); };
+			}
+			revers(*(mas + i), *mini);
+			mini = mas + i + 1;
+		}
+	}
+	template<typename S>
+	void sort_bis(S* mas, int len) {//bistraya sortirovka
+		if (!(len == 0 || len == 1)) {
+			bis_sor(mas, 0, len - 1);
+		}
+	}
+	template<typename S>
+	void sort_sli(S* mas, int len) {//sortirovka sliyaniyem
+		if (!(len == 0 || len == 1)) {
+			int* buffer = new int[len];
+			sli_sor(mas, buffer, 0, len - 1);
+			delete[] buffer;
+		}
+	}
+	//drugie poleznie funkcii
+	bool prost(int ch) {
+		if (ch < 0) { ch *= -1; }
+		int count = 0;
+		for (int i = 1; i < ch; i++) {
+			if (ch % i == 0) { count++; }
+		}
+		return (count == 1) ? (1) : (0);
+	}
+	template<typename F>
+	F fakt(F ch) {
+		F otv = 1;
+		for (int i = 1; i <= ch; i++) {
+			otv *= i;
+		}
+		return otv;
 	}
 }
